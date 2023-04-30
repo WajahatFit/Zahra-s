60755c817425c87a42f4293a850534e5ba1d34bf
Syed Wajahat Ali <wajahat.klk25.com>
2023-04-30 15:33:30 +0100

    adds heading

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { subHeading } from "../../components/SubHeading";
 
 import images from "../../constants";
 
 import "./Header.css";
 
-const Header = () => <div>Header</div>;
+const Header = () => (
+  <div className="app__header app__wrapper seection__padding" id="home">
+    <div className="app__wrapper_info">
+      <subHeading />
+    </div>
+    <div className="app__wrapper_image"></div>
+  </div>
+);
 
 export default Header;
