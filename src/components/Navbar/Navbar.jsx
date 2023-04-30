2c2c8c765ed1ce2342a55da9f3c0e70daa785397
Syed Wajahat Ali <wajahat.klk25.com>
2023-04-30 14:51:47 +0100

    navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,39 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { MdOutlineRestaurantenu } from "react-icons/md";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 import images from "../../constants/images";
 
 import "./Navbar.css";
 
-const Navbar = () => (
-  <nav className="app__navbar">
-    <div className="app__navbar-logo">
-      <h1 className="app__navbar-heading">ZAHRA'S</h1>
-    </div>
-    <ul className="app__navbar-links">
-      <li className="p__opensans">
-        <a href="#home">home</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#about">about</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#menu">menu</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#contact">contact</a>
-      </li>
-    </ul>
-    <div className="app__navbar-login">
-      <a href="#login" className="p__opensans">
-        Log In / Register
-      </a>
-      <div />
-      <a href="/" className="p__opensans">
-        Book Table
-      </a>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const [toggleMenu, setToogleMenu] = useState(false);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <h1 className="app__navbar-heading">ZAHRA'S</h1>
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans">
+          <a href="#home">home</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#about">about</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#menu">menu</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#contact">contact</a>
+        </li>
+      </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Log In / Register
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToogleMenu(true)}
+        />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToogleMenu(false)}
+            />
+            <ul className="app__navbar-smallscreen_links">
+              <li className="p__opensans">
+                <a href="#home">home</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#about">about</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#menu">menu</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#contact">contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import images from "../../constants";
 
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
+import "./Header.css";
+
+const Header = () => <div>Header</div>;
 
 export default Header;
