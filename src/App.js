b1d9a453452c3bc9fa40068b6d436001dfe1fef6
Syed Wajahat Ali <wajahat.klk25.com>
2023-05-03 13:42:25 +0100

    adds gallery section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
-import { Navbar } from './components';
-import './App.css';
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  SpecialMenu,
+} from "./container";
+import { Navbar } from "./components";
+import "./App.css";
 
 const App = () => (
   <div>
@@ -12,7 +21,6 @@ const App = () => (
     <SpecialMenu />
     <Chef />
     <Intro />
-    <Laurels />
     <Gallery />
     <FindUs />
     <Footer />
--- starter_src/constants/index.js
+++
@@ -1,5 +0,0 @@
-import images from "./images";
-import meat from "../assets/meat.mp4";
-import data from "./data";
-
-export { images, meat, data };
